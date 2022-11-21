575834b35c15b790ad143b6e4ade2dd9609ed777
jihyounchoi <jihyoun0403@gmail.com>
2022-11-21 13:58:59 +0900

    add requirements.txt

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ import json
 from data import Data
 import utils
 
-key = Data.key
+utils.return_ranks('973305588', '2021-09-04T08:40:24', '2022-09-04T08:40:24', key = Data.key)
 
-utils.return_ranks('973305588', '2021-09-04T08:40:24', '2022-09-04T08:40:24', key = key)
-# print(Data.flyingpets)
